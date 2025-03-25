dd42aa81af37e6018240f7c468701276a6923aef
Dikiy Perec <hopabsq@mail.ru>
2025-03-25 18:17:05 +0600

    Added switching between slides in horizontal

--- src/components/feed/userSlider.jsx
+++ src/components/feed/userSlider.jsx
@@ -1,19 +1,43 @@
-import { Swiper, SwiperSlide } from "swiper/react";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
-import "swiper/css";
+const images = [
+	'https://images.unsplash.com/photo-1580927752452-89d86da3fa0a',
+	'https://images.unsplash.com/photo-1606787366850-de6330128bfc',
+	'https://images.unsplash.com/photo-1544005313-94ddf0286df2'
+];
 
 export default function UserSlider() {
+	const [[page, direction], setPage] = useState([0, 0]);
+	
+	const navigate = (newDirection) => {
+		const newPage = (page + newDirection + images.length) % images.length;
+		setPage([newPage, newDirection]);
+	};
+
 	return (
-		<Swiper className='w-full h-full'>
-			<SwiperSlide>
-				<img src='https://images.unsplash.com/photo-1580927752452-89d86da3fa0a' alt='Slide 1-1' className='w-full h-full object-cover' />
-			</SwiperSlide>
-			<SwiperSlide>
-				<img src='https://images.unsplash.com/photo-1606787366850-de6330128bfc' alt='Slide 1-2' className='w-full h-full object-cover' />
-			</SwiperSlide>
-			<SwiperSlide>
-				<img src='https://images.unsplash.com/photo-1544005313-94ddf0286df2' alt='Slide 1-3' className='w-full h-full object-cover' />
-			</SwiperSlide>
-		</Swiper>
+		<div className='w-full h-full relative overflow-hidden'>
+			<div 
+				className='w-1/2 h-full absolute left-0 z-20 cursor-pointer' 
+				onClick={() => navigate(-1)} 
+			/>
+			<div 
+				className='w-1/2 h-full absolute right-0 z-20 cursor-pointer' 
+				onClick={() => navigate(1)} 
+			/>
+			
+			<AnimatePresence initial={false} mode="wait" custom={direction}>
+				<motion.img 
+					key={page}
+					src={images[page]} 
+					alt={`Slide ${page + 1}`}
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+					transition={{ duration: 0.075 }}
+					className='w-full h-full object-cover absolute top-0 left-0'
+				/>
+			</AnimatePresence>
+		</div>
 	);
 }
--- src/pages/feed.jsx
+++ src/pages/feed.jsx
@@ -7,15 +7,15 @@ export default function Home() {
 	return (
 		<div className='flex flex-col w-full h-full items-center justify-center'>
 			<Swiper direction={"vertical"} className='h-full w-full !pb-[4.65625rem]'>
-				<SwiperSlide className='bg-gray-100 flex items-center justify-center'>
+				<SwiperSlide className='flex items-center justify-center'>
 					<UserSlider />
 				</SwiperSlide>
 
-				<SwiperSlide className='bg-gray-200 flex items-center justify-center'>
+				<SwiperSlide className='flex items-center justify-center'>
 					<UserSlider />
 				</SwiperSlide>
 
-				<SwiperSlide className='bg-gray-300 flex items-center justify-center'>
+				<SwiperSlide className='flex items-center justify-center'>
 					<UserSlider />  
 				</SwiperSlide>
 			</Swiper>
