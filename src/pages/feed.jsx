da339db67a72fba58f76f92e038a1a3fe65bfd1e
pip64 <76626915+pip64@users.noreply.github.com>
2025-03-25 23:51:44 +0700

    Vertical blogs load

--- src/pages/feed.jsx
+++ src/pages/feed.jsx
@@ -8,19 +8,21 @@ import "swiper/css";
 export default function Home() {
 	const [users, setUsers] = useState([]);
 
-	useEffect(() => {
-		async function fetchPosts() {
-			const request = await api.v1.get("/post/get?after=0&region=slavic");
-			const localUsers = request.data?.success || [];
-			if (Object.keys(localUsers).length === 0) return;
-			setUsers(Object.keys(localUsers).map(user => localUsers[user]) || []);
-		}
+	async function fetchPosts() {
+		const allUsersIds = users.map(user => user.author.id);
+		const request = await api.v1.get(`/post/get?after=0&users=[${allUsersIds}]&region=slavic`);
+		const localUsers = request.data?.success || [];
+		if (Object.keys(localUsers).length === 0) return;
+		setUsers(users => [...users, ...Object.keys(localUsers).map(user => localUsers[user]) || []]);
+	}
 
+	useEffect(() => {
 		fetchPosts();
 	}, []);
+
 	return (
 		<div className='flex flex-col w-full h-full items-center justify-center'>
-			<Swiper direction={"vertical"} className='h-full w-full !pb-[calc(4.625rem+var(--safe-area-inset-bottom))]'>
+			<Swiper onSlideChange={(slide) => slide.activeIndex == users.length - 1 && fetchPosts()} direction={"vertical"} className='h-full w-full !pb-[calc(4.625rem+var(--safe-area-inset-bottom))]'>
 				{users.map((user, index) => (
 					<SwiperSlide key={index} className='flex items-center justify-center'>
 						<UserSlider user={user.author} posts={user?.posts} />
