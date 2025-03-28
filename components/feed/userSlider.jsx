049cc34781c71964a0b440d499d0bf9436bd11f7
pip64 <76626915+pip64@users.noreply.github.com>
2025-03-28 16:36:59 +0700

    fetch user posts (for horizontal slider)

--- components/feed/userSlider.jsx
+++ components/feed/userSlider.jsx
@@ -1,25 +1,17 @@
 import { useEffect, useState } from "react";
 import { AnimatePresence } from "framer-motion";
 import Post from "./post";
-import { api } from '../../lib/axios';
+import { useFetchUserPosts } from "../../hooks/useFetchUserPosts";
 
 export default function UserSlider({ user, posts, isActive }) {
 	const [page, setPage] = useState(0);
-	const [userPosts, setUserPosts] = useState([]);
+	const { userPosts, fetchPosts } = useFetchUserPosts(user, posts);
 
 	const navigate = direction => {
 		if (page + direction >= 0 && page + direction < userPosts.length) {
 			setPage(p => (p + direction) % userPosts.length);
 		}
 	};
-
-	async function fetchPosts() {
-		const lastBlog = userPosts[userPosts.length - 1];
-		const request = await api.v1.get(`/post/get?after=${lastBlog?.id}&user=${user.id}`);
-		setUserPosts(posts => [...posts, ...(request.data?.success || [])]);
-	}
-
-	useEffect(() => setUserPosts(posts), [posts]);
 
 	useEffect(() => {
 		if (page == userPosts.length - 2) fetchPosts();
---
+++ hooks/useFetchUserPosts.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+import { api } from "../lib/axios";
+
+export function useFetchUserPosts(user, posts) {
+    const [userPosts, setUserPosts] = useState([]);
+
+    async function fetchPosts() {
+		const lastBlog = userPosts[userPosts.length - 1];
+		const request = await api.v1.get(`/post/get?after=${lastBlog?.id}&user=${user.id}`);
+		setUserPosts(posts => [...posts, ...(request.data?.success || [])]);
+	}
+
+	useEffect(() => setUserPosts(posts), [posts]);
+
+    return { userPosts, fetchPosts };
+}
