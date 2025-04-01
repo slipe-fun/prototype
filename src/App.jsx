b4893faa35bfbe23ad50bfc683e3f8099682e5a3
Dikiy Perec <hopabsq@mail.ru>
2025-04-01 20:22:13 +0600

    Fix import path for PublishBlog component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/feed'
-import PublishBlog from './pages/publishBlog'
+import PublishBlog from './pages/PublishBlog'
 import NavBar from '../components/navbar'
 import Auth from './pages/auth'
 
