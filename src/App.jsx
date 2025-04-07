806831fc0806e8ed37cb9417cdecd5d094a5643c
Dikiy Perec <hopabsq@mail.ru>
2025-04-07 23:55:48 +0600

    Moved from RL to RN + Expo because RL sucks

--- src/App.jsx
+++
@@ -1,6 +0,0 @@
-import { Router } from './app/Router'
-import './app/styles/global.css'
-
-export function App() {
-  return <Router />
-}
--- src/shared/ui/Icon.jsx
+++
@@ -1,7 +0,0 @@
-import './Icon.css'
-
-export function Icon({ name, className = '', color = 'white', ...props }) {
-  return (
-    <view className={`icon icon-${name} ${className}`} style={{ backgroundColor: color }} {...props} />
-  )
-} 
